c60b8a6bdc5e35accbf412f323fa2a0a242a0dc0
ConvolutedDog <yangjianchao16@nudt.edu.cn>
2025-02-19 15:26:07 +0800

    [op] add op prod

---
+++ iterate-test/prod/process_prod.py
@@ -0,0 +1,105 @@
+import json
+
+import pandas as pd
+
+# ===================================
+input_log = "results/prod-result.txt"
+output_excel = (
+    "results/prod-result-512-512-8192-512-512-8192-512-512-8192-512-512-8192.csv"
+)
+# ===================================
+
+f = open(input_log, "r")
+json_data = f.readlines()
+f.close()
+
+df = pd.DataFrame(
+    columns=[
+        "op_name",
+        "dtype",
+        "mode",
+        "level",
+        "BLOCK_M",
+        "BLOCK_N",
+        "num_warps",
+        "legacy_shape",
+        "shape_detail_N",
+        "shape_detail_C",
+        "shape_detail_H",
+        "shape_detail_W",
+        "form_detail_dim",
+        "latency_base",
+        "latency",
+        "gbps_base",
+        "gbps",
+        "speedup",
+        "accuracy",
+        "tflops",
+        "utilization",
+        "latency_torch_compile",
+        "speedup_vs_torch_compile",
+        "latency_native_flaggems",
+        "speedup_vs_native_flaggems",
+        "speedup_vs_native_flaggems_trainset",
+        "error_msg",
+    ]
+)
+
+lines = json_data
+
+for line in lines:
+    if line.startswith("[INFO]"):
+        json_str = line.split("[INFO] ")[1]
+        data = json.loads(json_str)
+
+        # autotune_configs
+        autotune_configs = data["autotune_configs"]
+
+        # result
+        results = data["result"]
+
+        rows = []
+        for result in results:
+            shape_detail = result["shape_detail"]
+            row = {
+                "op_name": data["op_name"],
+                "dtype": data["dtype"],
+                "mode": data["mode"],
+                "level": data["level"],
+                "BLOCK_M": autotune_configs["BLOCK_M"],
+                "BLOCK_N": autotune_configs["BLOCK_N"],
+                "num_warps": autotune_configs["num_warps"],
+                "legacy_shape": result["legacy_shape"],
+                "shape_detail_N": shape_detail["input"][0],
+                "shape_detail_C": shape_detail["input"][1],
+                "shape_detail_H": shape_detail["input"][2],
+                "shape_detail_W": shape_detail["input"][3],
+                "form_detail_dim": shape_detail["dim"]
+                if "dim" in shape_detail
+                else None,
+                "latency_base": result["latency_base"],
+                "latency": result["latency"],
+                "gbps_base": result["gbps_base"],
+                "gbps": result["gbps"],
+                "speedup": result["speedup"],
+                "accuracy": result["accuracy"],
+                "tflops": result["tflops"],
+                "utilization": result["utilization"],
+                "latency_torch_compile": result["latency_torch_compile"],
+                "speedup_vs_torch_compile": result["speedup_vs_torch_compile"],
+                "latency_native_flaggems": result["latency_native_flaggems"],
+                "speedup_vs_native_flaggems": result["speedup_vs_native_flaggems"],
+                "speedup_vs_native_flaggems_trainset": result[
+                    "speedup_vs_native_flaggems_trainset"
+                ],
+                "error_msg": result["error_msg"],
+            }
+
+            # df = df.append(row, ignore_index=True)
+            rows.append(row)
+
+        df = pd.concat([df, pd.DataFrame(rows)], ignore_index=True)
+
+df.to_csv(output_excel, index=False)
+
+print("Written to " + output_excel)
---
+++ iterate-test/prod/start_pytest_eval_train_model.py
@@ -0,0 +1,297 @@
+import importlib
+import os
+import subprocess
+import sys
+from collections import defaultdict
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from performance_utils import (
+    ShapeGenerator,
+    TunedConfigGenerator,
+    archive_file_with_timestamp,
+    print_centered_label,
+    read_config_from_yaml,
+    run_perf_pytest,
+    write_config_to_yaml,
+    write_shapes_to_yaml,
+)
+
+# flake8: noqa: E402
+import flag_gems
+
+# ===---------------------------------------------------------------------------------===
+# User-Specified Parameters
+# ===---------------------------------------------------------------------------------===
+
+pytest_operation_name = "prod"
+# Optional["float16", "float32", "bfloat16", "int16", "int32", "bool", "cfloat"]
+pytest_data_type = "float16"
+
+pytest_verbose = True
+pytest_warmup_runs = 3
+pytest_iter_runs = 3
+
+# Just don't edit this.
+pytest_shape_file = "configs/shape.yaml"
+
+# Just don't edit this.
+print_shape_config_combinations = True
+print_grouped_shape_config_combinations = False
+
+
+# ===---------------------------------------------------------------------------------===
+# Configuration Dictionary for Reading Native Flaggems from Training Set
+# ===---------------------------------------------------------------------------------===
+# This section defines a configuration dictionary (`excel_config`) that specifies the
+# column names and structure of the Excel files containing native flaggems performance.
+# The dictionary is used by the `read_native_flaggems_from_trainset` function to read and
+# process the training set data. And the main purpose of this function is to prevent the
+# performance of FlagGems that have already been tested from being retested, and also to
+# prevent combinations of shapes and configs that have already been traversed from being
+# re-traversed.
+# ===---------------------------------------------------------------------------------===
+
+# NOTE: These column names must exactly match the names in the XLSX file in the tarinset
+# directory. These columns of "dtype_col" and "shape_cols" will be used as the key to
+# perform the functionality introduced above.
+excel_config = {
+    # Data type.
+    "dtype_col": "dtype",
+    # Shape parameters.
+    "shape_cols": [
+        "shape_detail_N",
+        "shape_detail_C",
+        "shape_detail_H",
+        "shape_detail_W",
+    ],
+    # In addition to the shape parameters defined in the yaml file, there are also some
+    # parameters that are some parameters that are form parameters of functions, such as
+    # the `dim` parameter of the `mean` operator. When dealing with form parameters, I
+    # still treat them as shape parameters for simplicity.
+    "form_cols": ["form_detail_dim"],
+    # Auto-tune configs.
+    "config_cols": [
+        "BLOCK_M",
+        "BLOCK_N",
+        "num_warps",
+    ],
+    # Performance.
+    "latency_col": "latency",
+    # Benchmark name of shape yaml.
+    "bench_name": "PRODBenchmark",
+    # Shape description of shape yaml. It should correspond one-to-one with "shape_cols".
+    "shape_desc": ["N", "C", "H", "W"],
+}
+config_format = read_config_from_yaml(pytest_operation_name)
+print(f"Using config format of {config_format} to write configs.")
+
+
+# ===---------------------------------------------------------------------------------===
+# Result File Management and Archiving
+# ===---------------------------------------------------------------------------------===
+# This section handles the creation and archiving of result files for a given operation_name.
+# The result file is named based on the operation_name and stored in the "results/" directory.
+# After generating the result file, it is archived with a timestamp to preserve historical
+# results and avoid overwriting previous data. This ensures that each run's results are
+# uniquely stored and can be referenced later for comparison or analysis.
+#
+# The `archive_file_with_timestamp` function is responsible for adding a timestamp to the
+# result file and moving it to an archive directory. This helps maintain a clean and
+# organized record of all performance testing results.
+# ===---------------------------------------------------------------------------------===
+
+# Define the path to the result file based on the operation_name name
+result_file = "results/" + pytest_operation_name + "-result.txt"
+
+# Archive the result file with a timestamp to preserve historical results
+archive_file_with_timestamp(result_file)
+
+
+# ===---------------------------------------------------------------------------------===
+# Shape Generator Functions
+# ===---------------------------------------------------------------------------------===
+# This section contains functions that generate various parameters for shape details and
+# configuration options. These parameters are used to create different configurations for
+# performance testing. Each function generates a list of possible values for a specific
+# parameter, which are then combined to form complete configuration sets. The purpose of
+# these generators is to systematically explore the parameter space and identify optimal
+# configurations for the given task.
+# ===---------------------------------------------------------------------------------===
+
+
+# NOTE: The function name must start with "gen_", and the second half of the name must
+# correspond to the name in "Shape parameters" and "Auto-tune configs" in excel_config.
+def gen_shape_detail_N():
+    # return list(range(512, 8192 + 1, 512))
+    return [2]
+
+
+def gen_shape_detail_C():
+    # return list(range(512, 8192 + 1, 512))
+    return [3]
+
+
+def gen_shape_detail_H():
+    # return list(range(512, 8192 + 1, 512))
+    return [64]
+
+
+def gen_shape_detail_W():
+    # return list(range(512, 8192 + 1, 512))
+    return [64]
+
+
+def gen_form_detail_dim():
+    return [None, 0, 1, 2, 3]
+
+
+# ===---------------------------------------------------------------------------------===
+# Configuration Parameter Generation Functions
+# ===---------------------------------------------------------------------------------===
+# This section contains functions to generate configuration parameters (e.g., block sizes,
+# split factors, number of stages, etc.) based on input shapes (M, K, N). These functions
+# use predefined formulas to calculate optimal values for each parameter.
+# ===---------------------------------------------------------------------------------===
+
+
+# Define functions to generate parameters
+def gen_BLOCK_M(
+    shape_detail_N, shape_detail_C, shape_detail_H, shape_detail_W, form_detail_dim
+):
+    """
+    block m: 5.02754660e-5  1.24650843e-05 5.70746029e-05 0.951953125 5
+    """
+    res = 5.02754660e-5 * shape_detail_N + 5.70746029e-05 * shape_detail_C + 0.951953125
+    return 2 ** (round(res + 5))
+
+
+def gen_BLOCK_N(
+    shape_detail_N, shape_detail_C, shape_detail_H, shape_detail_W, form_detail_dim
+):
+    """
+    block m: 5.02754660e-5  1.24650843e-05 5.70746029e-05 0.951953125 5
+    """
+    res = 5.02754660e-5 * shape_detail_N + 5.70746029e-05 * shape_detail_C + 0.951953125
+    return 2 ** (round(res + 5))
+
+
+def gen_num_warps(
+    shape_detail_N, shape_detail_C, shape_detail_H, shape_detail_W, form_detail_dim
+):
+    """
+    num warps: -1.61563649e-05 2.72414264e-05 -2.95751235e-05 1.27919921875 1
+    """
+    res = (
+        -1.61563649e-05 * shape_detail_N
+        + -2.95751235e-05 * shape_detail_C
+        + 1.27919921875
+    )
+    return 2 ** (
+        round(res + 1) + (form_detail_dim if form_detail_dim is not None else 0)
+    )
+
+
+# ===---------------------------------------------------------------------------------===
+# Shape and Configuration Generators for Performance Testing
+# ===---------------------------------------------------------------------------------===
+# This section initializes the `ShapeGenerator` and `ConfigGenerator` classes, which are
+# responsible for generating combinations of shapes and configurations for performance
+# testing. These generators use the parameter generation functions defined earlier to
+# create a comprehensive set of test cases. The `excel_config` dictionary is passed to
+# both generators to ensure consistency with the training set data and to avoid retesting
+# previously evaluated combinations.
+# ===---------------------------------------------------------------------------------===
+
+shapegen = ShapeGenerator(
+    excel_config,
+    (gen_shape_detail_N, gen_shape_detail_C, gen_shape_detail_H, gen_shape_detail_W),
+    form_generators=[gen_form_detail_dim],
+)
+
+# print(shapegen.generate())
+# for kv in shapegen.generate():
+#     print(kv)
+
+tunedconfiggen = TunedConfigGenerator(
+    excel_config,
+    (gen_BLOCK_M, gen_BLOCK_N, gen_num_warps),
+    shapegen,
+)
+
+shape_config_combinations = tunedconfiggen.generate()
+
+if print_shape_config_combinations:
+    for shape, config in shape_config_combinations:
+        print(shape, config)
+
+
+# ===---------------------------------------------------------------------------------===
+# Group Shape-Config Combinations by Config
+# ===---------------------------------------------------------------------------------===
+# This section groups shape-config combinations by their configuration (`config_cols`).
+# Each unique configuration will correspond to a list of shapes (`shape_cols`), allowing
+# for efficient batch processing of shapes with the same configuration.
+# ===---------------------------------------------------------------------------------===
+
+
+# Helper function to extract config from a shape-config pair
+def extract_config(pair, excel_config):
+    shape, config = pair
+    return {key: config[key] for key in excel_config["config_cols"]}
+
+
+# Group shape-config combinations by config
+grouped_shape_config_combinations = defaultdict(list)
+for pair in shape_config_combinations:
+    config_key = extract_config(pair, excel_config)
+    grouped_shape_config_combinations[tuple(config_key.items())].append(pair)
+
+
+# ===---------------------------------------------------------------------------------===
+# Run Performance Tests
+# ===---------------------------------------------------------------------------------===
+# This script processes each unique configuration and its associated shapes, writes them
+# to YAML files, runs performance tests, and appends the results to a main result file.
+# The "START" and "END" labels help visually separate the output for each configuration.
+# ===---------------------------------------------------------------------------------===
+
+# Iterate over each unique configuration
+for config_key in grouped_shape_config_combinations:
+    print_centered_label(" START ", color="green")
+
+    # Convert the config_key (tuple of items) back to a dictionary
+    config_dict = dict(config_key)
+    print(f"Config: {config_dict}")
+    # Write the configuration to YAML
+    write_config_to_yaml(config_dict)
+
+    # Reload the configuration and the operation
+    flag_gems.runtime.config_loader = flag_gems.runtime.ConfigLoader.reset_instance()
+    importlib.reload(sys.modules["flag_gems.ops." + pytest_operation_name])
+
+    # Write the shapes to YAML
+    write_shapes_to_yaml(
+        [shape_pair[0] for shape_pair in grouped_shape_config_combinations[config_key]],
+        excel_config,
+        output_path=pytest_shape_file,
+    )
+
+    if print_grouped_shape_config_combinations:
+        # Iterate over each shape pair for the current configuration
+        for shape_pair in grouped_shape_config_combinations[config_key]:
+            shape = shape_pair[0]  # Extract the shape dictionary
+            print(f"Shape: {shape}")
+
+    # Run pytest
+    output_file = run_perf_pytest(
+        operation=pytest_operation_name,
+        shape_file=pytest_shape_file,
+        warmup=pytest_warmup_runs,
+        iter=pytest_iter_runs,
+        dtypes=pytest_data_type,
+        verbose=pytest_verbose,
+    )
+    subprocess.run("cat " + output_file + " >> " + result_file, shell=True, check=False)
+
+    print_centered_label(" END ", color="green")
